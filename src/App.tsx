142412d4352461e09db6b1e985fe32c088882738
SamHalera <samuel.halera@gmail.com>
2024-07-15 11:16:51 +0200

    install and config daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-primary">
+        Hello world!
+      </h1>
+      <button className="btn btn-outline btn-info">Info</button>
+      <button className="btn btn-outline btn-success">Success</button>
+      <button className="btn btn-outline btn-warning">Warning</button>
+      <button className="btn btn-outline btn-error">Error</button>
     </div>
   );
 }
