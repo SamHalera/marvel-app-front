aa1e994e7cbae41a12ad0c1da59a925b60b54fd4
SamHalera <samuel.halera@gmail.com>
2024-07-15 11:52:15 +0200

    copy images assets, create router system basic

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="flex h-32  items-center justify-center p-4 px-4 text-white">
+      <p className=" text-center text-xl">
+        Made with love 💙 and passion 🚀 by
+        <a href="https://github.com/SamHalera"> Samuel H. Barubiriza</a>
+      </p>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/pages/Comics.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Comics = () => {
+  return <div>COMICS PAGE</div>;
+};
+
+export default Comics;
