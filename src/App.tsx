7a049f82aa2b9460388cc879d5eb03fa62b90f62
SamHalera <samuel.halera@gmail.com>
2024-07-15 11:29:13 +0200

    edit them

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ function App() {
       <h1 className="text-3xl font-bold underline text-primary">
         Hello world!
       </h1>
-      <button className="btn btn-outline btn-info">Info</button>
+      <button className="btn btn-outline btn-secondary rounded-none">
+        Info
+      </button>
       <button className="btn btn-outline btn-success">Success</button>
       <button className="btn btn-outline btn-warning">Warning</button>
       <button className="btn btn-outline btn-error">Error</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ed1d24",
+      },
+      btn: {
+        borderRadius: 0,
+      },
+    },
   },
   daisyui: {
     themes: ["dark"],
