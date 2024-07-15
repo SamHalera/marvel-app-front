e0c6ddca89958b84f4c66390db726d5fd556248c
SamHalera <samuel.halera@gmail.com>
2024-07-15 11:07:58 +0200

    install and confi tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
