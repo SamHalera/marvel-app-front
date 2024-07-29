bbda9ef92d7a58f2817fce4b0aeec9d8ee2302a6
SamHalera <samuel.halera@gmail.com>
2024-07-29 11:34:48 +0200

    correction baseApiUrl

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,2 @@
-export const baseAPIUrl = "http://localhost:3000";
-// const baseUrl = "https://site--backend-marvel--v5zlz7yt85wg.code.run";
+// export const baseAPIUrl = "http://localhost:3000";
+export const baseAPIUrl = "https://site--backend-marvel--v5zlz7yt85wg.code.run";
