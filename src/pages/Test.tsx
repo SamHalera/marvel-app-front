eb9afda108d10c29b18aa074ee5d24225e16bdc5
SamHalera <samuel.halera@gmail.com>
2024-11-12 12:13:44 +0100

    corrections for build

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -21,6 +21,7 @@ const Test = () => {
       console.log("data==>", data);
     };
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <div>TEST</div>;
 };
